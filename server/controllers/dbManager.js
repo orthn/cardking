1d66498d15d2fd26b0f29cff5b4a730ac7d86324
nicoorth <nico.orth@stud.fh-campuswien.ac.at>
2024-12-20 21:15:17 +0100

    updated error handling in dbManager

--- server/controllers/dbManager.js
+++ server/controllers/dbManager.js
@@ -3,18 +3,10 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const connectDB = async () => {
-    try {
-
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
-        console.log('MongoDB Atlas connected.');
-    } catch (err) {
-        console.log("Mongo URI:", process.env.MONGO_URI);
-        console.error('Database connection error:', err.message);
-        process.exit(1); // Exit the app if unable to connect
-    }
+    await mongoose.connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
 };
 
 module.exports = connectDB;
