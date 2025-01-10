0ea796dca0d5e5760ecd62551f5e381587c5a5b3
nicoorth <nico.orth@stud.fh-campuswien.ac.at>
2025-01-10 23:17:37 +0100

    added Export endpoint

--- server/routes/cards.js
+++ server/routes/cards.js
@@ -2,7 +2,7 @@ let express = require('express')
 let router = express.Router()
 
 let Card = require("../models/cardSchema")
-let Category = require("../models/categorySchema");
+let Category = require("../models/categorySchema")
 
 router.get('/', function (req, res) {
     res.send("WIP: Default Cards Page")
@@ -33,26 +33,26 @@ router.get('/get', async function (req, res) {
  * POST: localhost:3000/cards/create
  */
 router.post('/create', async function (req, res) {
-    const { question, type, answers, correctAnswer, category } = req.body;
+    const {question, type, answers, correctAnswer, category} = req.body
 
     // get userID from session
-    const userId = req.session.userId;
+    const userId = req.session.userId
 
     if (!userId) {
-        return res.status(401).send("User is not authenticated. Please log in.");
+        return res.status(401).send("User is not authenticated. Please log in.")
     }
 
-    if (!question) return res.status(400).send("Question is required");
-    if (!type) return res.status(400).send("Type is required");
-    if (!answers) return res.status(400).send("Answers are required");
-    if (!correctAnswer) return res.status(400).send("CorrectAnswer is required");
+    if (!question) return res.status(400).send("Question is required")
+    if (!type) return res.status(400).send("Type is required")
+    if (!answers) return res.status(400).send("Answers are required")
+    if (!correctAnswer) return res.status(400).send("CorrectAnswer is required")
 
     try {
         // 1. Find or create category
         let categoryDoc = await Category.findOneAndUpdate(
-            { category, userId },
-            {$inc: { cardCount: 1 } },  // Increment cardCount by 1
-            null);
+            {category, userId},
+            {$inc: {cardCount: 1}},  // Increment cardCount by 1
+            null)
 
 
         if (!categoryDoc) {
@@ -61,22 +61,22 @@ router.post('/create', async function (req, res) {
                 category,
                 cardCount: 1,
                 userId
-            });
-            await categoryDoc.save();
+            })
+            await categoryDoc.save()
         }
 
-        categoryId = categoryDoc._id;
+        categoryId = categoryDoc._id
 
         // 3. create the card and hand over the category ID
-        const card = new Card({ question, type, answers, correctAnswer, categoryId });
-        await card.save();
+        const card = new Card({question, type, answers, correctAnswer, categoryId})
+        await card.save()
 
 
-        console.log("Card created and added to category successfully:", card);
-        res.status(201).send({ message: "Card created successfully", card });
+        console.log("Card created and added to category successfully:", card)
+        res.status(201).send({message: "Card created successfully", card})
     } catch (error) {
-        console.error("Error saving card or category:", error);
-        res.status(500).send({ message: "Error: " + error.message });
+        console.error("Error saving card or category:", error)
+        res.status(500).send({message: "Error: " + error.message})
     }
 })
 
@@ -137,38 +137,36 @@ function validateCardType(type, answers, correctAnswer) {
  */
 router.get('/category', async function (req, res) {
     try {
-        const userId = req.session.userId;
+        const userId = req.session.userId
+        const category = req.query.category
 
-        const category = req.query.category;
-
-
-        let cards;
-        let categoryEntries;
+        let cards
+        let categoryEntries
 
         if (!category) {
-            categoryEntries = await Category.find({ userId });
+            categoryEntries = await Category.find({userId})
 
             if (!categoryEntries || categoryEntries.length === 0) {
-                return res.status(404).json({ message: "No categories found" });
+                return res.status(404).json({message: "No categories found"})
             }
 
-            const categoryIds = categoryEntries.map(category => category._id);
-            cards = await Card.find({ categoryId: { $in: categoryIds } });
+            const categoryIds = categoryEntries.map(category => category._id)
+            cards = await Card.find({categoryId: {$in: categoryIds}})
         } else {
-            //categoryEntries = await Category.findOne({ category, userId });
+            //categoryEntries = await Category.findOne({ category, userId })
 
             //if (!categoryEntries) {
-            //    return res.status(404).json({ message: "Category not found" });
+            //    return res.status(404).json({ message: "Category not found" })
             //}
 
-            cards = await Card.find({ categoryId: category });
-            console.log(cards);
+            cards = await Card.find({categoryId: category})
+            console.log(cards)
         }
 
         return res.status(200).send(cards)
     } catch (err) {
-        console.error(err);
-        return res.status(500).send({ message: 'Internal server error', error: err.message });
+        console.error(err)
+        return res.status(500).send({message: 'Internal server error', error: err.message})
     }
 })
 
@@ -179,33 +177,68 @@ router.get('/category', async function (req, res) {
  */
 router.delete('/delete/:id', async function (req, res) {
 
-    const { id } = req.params;
+    const {id} = req.params
 
-    console.log('Received ID to delete:', id);
+    console.log('Received ID to delete:', id)
 
     try {
-        const deletedCard = await Card.findByIdAndDelete(id);
+        const deletedCard = await Card.findByIdAndDelete(id)
 
         if (!deletedCard) {
-            return res.status(404).send({ message: `Card with ID ${id} not found.` });
+            return res.status(404).send({message: `Card with ID ${id} not found.`})
         }
 
         await Category.findByIdAndUpdate(
             deletedCard.categoryId,
-            { $inc: { cardCount: -1 } }
-        );
+            {$inc: {cardCount: -1}}
+        )
 
-        console.log('Card deleted successfully:', deletedCard);
+        console.log('Card deleted successfully:', deletedCard)
 
-        return res.status(200).send({ message: "Card deleted successfully.", deletedCard });
+        return res.status(200).send({message: "Card deleted successfully.", deletedCard})
     } catch (error) {
-        console.error('Error deleting card:', error);
-        return res.status(500).send({ message: "Error deleting card: " + error.message });
+        console.error('Error deleting card:', error)
+        return res.status(500).send({message: "Error deleting card: " + error.message})
     }
-});
+})
 
+router.get('/export', async function (req, res) {
+    try {
+        const userId = req.session.userId
+        const {categoryId} = req.body
 
+        let cards
+        let category
 
+        category = await Category.find({userId: userId, _id: categoryId}, null, null)
+        if (!category) {
+            return res.status(404).send({message: "No categories found"})
+        }
+
+        cards = await Card.find({ categoryId: { $in: categoryId } }, null, null)
+        if (!cards || cards.length === 0) {
+            return res.status(404).send({message: `No cards found.`})
+        }
+
+        const formatted = {
+            cards: cards.map(card => ({
+                id: card._id.toString(),
+                category: category.category,
+                type: card.type,
+                question: card.question,
+                answers: card.answers,
+                correctAnswer: card.correctAnswer,
+            }))
+        }
+
+        res.setHeader('Content-Disposition', 'attachment filename="cards.json"')
+        res.setHeader('Content-Type', 'application/json')
+        return res.status(200).send(JSON.stringify(formatted, null, 2))
+    } catch (error) {
+        console.error("Error exporting cards of category", error)
+        return res.status(500).send({message: 'Error exporting cards of category', error: error.message})
+    }
+})
 
 
 module.exports = router
